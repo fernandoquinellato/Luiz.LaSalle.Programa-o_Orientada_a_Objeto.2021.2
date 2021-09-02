760244b02ae8649e103eec71f7208fb0c1edb370
fernandoquinellato <42873800+fernandoquinellato@users.noreply.github.com>
2021-09-02 10:17:52 -0300

    Create BikeTester.java

---
+++ BikeTester.java
@@ -0,0 +1,18 @@
+public class BikeTester {
+
+    public static void main(String[] args) {
+        
+        Bike bik1 = new Bike(); 
+
+        bik1.marca = "Caloi";
+        bik1.cor = "Azul";
+        bik1.qtdMarchas.velocidade="1";
+        
+
+        Bik1.andar();
+        Bik1.frear()
+
+
+    }
+    
+}
