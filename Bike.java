206241397c20f96966d52d5e66da782ad23545db
fernandoquinellato <42873800+fernandoquinellato@users.noreply.github.com>
2021-09-02 10:05:50 -0300

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,27 @@
+public class Bike {
+
+    //atributos
+    public String marca;
+    public String cor;
+    public Time qtdMarchas;
+   
+
+    //construtor
+    public Bike(){
+        qtdMarchas = new Time();
+    }
+
+    
+    //metodos
+    public void andar()
+    {
+        if(qtdMarchas<1) qtdMarchas+1;
+        System.out.println( this.marca + " andar " );
+    }
+
+    public void freiar()
+    {
+        if(qtdMarchas>1) qtdMarchas-1;
+        System.out.println( this.marca + " freiar " );
+    }
+}
